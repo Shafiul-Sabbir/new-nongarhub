c541f1e3086351f430f4c1a293a159f1e628239b
Shafiul-Sabbir <sabbirvai82@gmail.com>
2023-10-18 20:36:26 +0600

    slash

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
      path('', views.dashboard, name='Dashboard'),
-     path('home/', views.home, name='Home'),
-     path('blog/', views.blog, name='Blog'),
-     path('about/',views.about, name='About'),
+     path('home', views.home, name='Home'),
+     path('blog', views.blog, name='Blog'),
+     path('about',views.about, name='About'),
      path('contact',views.contact, name='Contact'),
      
 ]
